0f4b8371aec3a1edb7171d2341e883adc2e37407
Matt Goo <matthew.goo@points.com>
2017-11-16 14:45:36 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { getBrowserOrientation, getCurrentLocation } from './browserUtils';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.onHeadingChange = this.onHeadingChange.bind(this);
+    this.state = {
+      heading: 0
+    };
+  }
+
+  componentDidMount() {
+    getCurrentLocation();
+    window.addEventListener("deviceorientation", this.onHeadingChange);
+  }
+
+  onHeadingChange(event) {
+    var heading = event.alpha;
+    const { width, height } = window.screen;
+    const defaultOrientation = width > height ? 'landscape' : 'portrait';
+
+    if (typeof event.webkitCompassHeading !== "undefined") {
+      heading = event.webkitCompassHeading; //iOS non-standard
+    }
+
+    var orientation = getBrowserOrientation();
+
+    if (typeof heading === "undefined" || heading === null) return;
+
+    // what adjustment we have to add to rotation to allow for current device orientation
+    var adjustment = 0;
+    if (defaultOrientation === "landscape") {
+      adjustment -= 90;
+    }
+
+    if (typeof orientation !== "undefined") {
+      var currentOrientation = orientation.split("-");
+
+      if (defaultOrientation !== currentOrientation[0]) {
+        if (defaultOrientation === "landscape") {
+          adjustment -= 270;
+        } else {
+          adjustment -= 90;
+        }
+      }
+
+      if (currentOrientation[1] === "secondary") {
+        adjustment -= 180;
+      }
+    }
+
+    const hng = heading + adjustment;
+    const phase = hng < 0 ? 360 + hng : hng;
+    const textContent = (360 - phase | 0) + "°";
+
+    this.setState({ heading: hng });
+  }
+
+  render() {
+    const { heading } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          <input
+            type="file"
+            accept="image/*"
+            capture="camera"
+            onChange={(event) => {
+              getCurrentLocation().then(coords => {
+                alert('WOOF')
+                const { lat, lng } = coords;
+                alert([lat, lng, heading])
+                // fetch('/', {
+                //   method: 'POST',
+                //   body: {
+                //     lat, lng, heading
+                //   }
+                // })
+              });
+            }}
+          />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/browserUtils.js
@@ -0,0 +1,61 @@
+
+
+// browser agnostic orientation
+export const getBrowserOrientation = () => {
+  const { orientation, mozOrientation, msOrientation } = window.screen;
+  if (orientation && orientation.type) {
+    return orientation.type;
+  }
+  return orientation ||
+                mozOrientation ||
+                msOrientation;
+
+  /*
+    'portait-primary':      for (screen width < screen height, e.g. phone, phablet, small tablet)
+                              device is in 'normal' orientation
+                            for (screen width > screen height, e.g. large tablet, laptop)
+                              device has been turned 90deg clockwise from normal
+    'portait-secondary':    for (screen width < screen height)
+                              device has been turned 180deg from normal
+                            for (screen width > screen height)
+                              device has been turned 90deg anti-clockwise (or 270deg clockwise) from normal
+    'landscape-primary':    for (screen width < screen height)
+                              device has been turned 90deg clockwise from normal
+                            for (screen width > screen height)
+                              device is in 'normal' orientation
+    'landscape-secondary':  for (screen width < screen height)
+                              device has been turned 90deg anti-clockwise (or 270deg clockwise) from normal
+                            for (screen width > screen height)
+                              device has been turned 180deg from normal
+  */
+
+}
+
+
+export const getCurrentLocation = () => {
+
+  return new Promise(resolve => {
+
+    if (navigator.geolocation) {
+      console.log('Geolocation is supported!');
+    } else {
+      alert('Geolocation is not supported for this Browser/OS version yet.');
+      resolve(null);
+      return;
+    }
+    try {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+
+        resolve({
+          lat: latitude,
+          lng: longitude
+        });
+      });
+    } catch(err) {
+      alert(err)
+    }
+
+  });
+
+};
