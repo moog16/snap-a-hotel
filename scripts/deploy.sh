0e7c3056b2d9daf7ce289ccee49852602cbcc414
Matt Goo <matthew.goo@points.com>
2017-11-22 16:14:21 -0800

    remove cd from deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,5 @@
 git pull;
 yarn;
 yarn build;
-cd /usr/share/nginx/snapit.live/;
-rm -rf *;
-cp -rf /home/snap-a-hotel/build/ /usr/share/nginx/snapit.live/;
+rm -rf /usr/share/nginx/snapit.live/;
+cp -rf ./build/ /usr/share/nginx/snapit.live/;
